bc2d801f9f0e52e831fd55317661e13538352749
Murillo <murilloalvesbr34@gmail.com>
2026-03-12 09:40:51 -0300

    arquivo do servidor

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    console.log("certo");
+    res.end('certo');
+});
+
+server.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
